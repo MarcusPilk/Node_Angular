710e884eb810dcae93bb5699a7d1f5eeb7387d0c
marcuspilkington <marcuspilkington@outlook.com>
2019-01-15 16:33:13 +0000

    Send file not read file

--- Node/server.js
+++ Node/server.js
@@ -1,19 +1,21 @@
 let express = require('express');
 let app = new express();
+let path = require('path');
 const fs = require('fs');
 
 
 app.get('/', (req,res) => {
-    fs.readFile('../WebApp/index.html', (err,data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        if(err){
-            res.send(err);
-            res.end();
-        }else{
-            res.write(data);
-            res.end()
-        }
-    });
+    // fs.readFile('../WebApp/index.html', (err,data) => {
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     if(err){
+    //         res.send(err);
+    //         res.end();
+    //     }else{
+    //         res.write(data);
+    //         res.end()
+    //     }
+    // });
+    res.sendFile(path.join(__dirname + '/../WebApp/index.html'));
 });
 
 app.listen(3001);

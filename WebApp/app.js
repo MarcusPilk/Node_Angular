af61f546f3772d2d52b005acbf37cef1b835bc88
marcuspilkington <marcuspilkington@outlook.com>
2019-01-17 17:11:52 +0000

    ChromeOS

--- WebApp/app.js
+++ WebApp/app.js
@@ -3,27 +3,27 @@ const KELVIN = 273.15;
 
 app.controller("WeatherController",['$scope','$http', '$sce',function ($scope, $http, $sce) {
 
-    $scope.demo = "Weather API";
+    $scope.demo = "ChromeOS - Marcus Pilkington";
 
-    $scope.city = "London";
-    $scope.country = "uk";
-
-    let weatherAPIUrl = $sce.trustAsResourceUrl('http://localhost:3001/');
-
-    $http.get(weatherAPIUrl).then(resp => {
-        let data = resp.data;
-
-        $scope.weather = {
-            main: data.weather[0].main,
-            description: data.weather[0].description
-        };
-
-        $scope.temp = {
-            currentTemp: data.main.temp - KELVIN,
-            minTemp: data.main.temp_min - KELVIN,
-            maxTemp: data.main.temp_max - KELVIN
-        };
-    });
+    // $scope.city = "London";
+    // $scope.country = "uk";
+    //
+    // let weatherAPIUrl = $sce.trustAsResourceUrl('http://localhost:3001/');
+    //
+    // $http.get(weatherAPIUrl).then(resp => {
+    //     let data = resp.data;
+    //
+    //     $scope.weather = {
+    //         main: data.weather[0].main,
+    //         description: data.weather[0].description
+    //     };
+    //
+    //     $scope.temp = {
+    //         currentTemp: data.main.temp - KELVIN,
+    //         minTemp: data.main.temp_min - KELVIN,
+    //         maxTemp: data.main.temp_max - KELVIN
+    //     };
+    // });
 
 
 
